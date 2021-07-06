d58c3037c19eaa61b448030560ce94b30fdd5869
José Sérgio Mendes Pereira Junior <jose.mendes@sapium.com.br>
2021-07-06 13:46:52 -0300

    app js

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef, useEffect } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  ActivityIndicator,
+  BackHandler,
+} from "react-native";
+
+import { WebView } from "react-native-webview";
+
+// const WEBVIEW_URL = 'https://www.sapium.com.br/';
+const WEBVIEW_URL = "http://192.168.0.153:8002";
 
 export default function App() {
+  const [canGoBack, setCanGoBack] = useState(false);
+  const [canGoForward, setCanGoForward] = useState(false);
+  const [currentUrl, setCurrentUrl] = useState("");
+  const webviewRef = useRef(null);
+
+  console.log(canGoBack, canGoForward, currentUrl);
+
+  const onAndroidBackPress = () => {
+    if (webviewRef.current) {
+      webviewRef.current.goBack();
+      return true; // prevent default behavior (exit app)
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener("hardwareBackPress", onAndroidBackPress);
+
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", onAndroidBackPress);
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <SafeAreaView style={styles.flexContainer}>
+        <WebView
+          onMessage={(event) => console.log(event.nativeEvent)}
+          userAgent="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko"
+          ref={webviewRef}
+          onNavigationStateChange={(navState) => {
+            setCanGoBack(navState.canGoBack);
+            setCanGoForward(navState.canGoForward);
+            setCurrentUrl(navState.url);
+          }}
+          source={{ uri: WEBVIEW_URL }}
+          startInLoadingState={true}
+          renderLoading={() => (
+            <ActivityIndicator
+              color="black"
+              size="large"
+              style={styles.flexContainer}
+            />
+          )}
+        />
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  flexContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
